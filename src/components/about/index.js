51bf6dd429ba777c763bea183560c20cf5276268
Musa Bas <64317827+musabash@users.noreply.github.com>
2021-06-29 15:02:43 +0100

    Update index.js

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -17,7 +17,7 @@ export default function About() {
       <p>I love building web apps and sites with <span>React JS</span>.</p>
       <p>I have a passion for coding. I always try to look to learn and stay current with developments in relevant technologies.
       </p>
-      <p>I've built a strong professional background with a Bachelor's degree focused in Physics from Marmara University but my passion for coding made me chose this new career path. 5+ years of experience in <span>IT & services industry</span> is helping me a lot to adapt quickly to new technologies and to solidify my knowledge.</p>
+      <p>I've built a strong professional background with a Bachelor's degree focused in Physics from Marmara University but my passion for coding made me chose this new career path. 3+ years of experience in <span>IT & services industry</span> is helping me a lot to adapt quickly to new technologies and to solidify my knowledge.</p>
     </div>
   )
 }
