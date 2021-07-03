483dbf6193cea7cfeb8c3d063cbee439bf0e15b8
Musa Bas <64317827+musabash@users.noreply.github.com>
2021-07-03 16:53:26 +0100

    project image link changed from url to iframe

--- src/components/project-link/index.js
+++ src/components/project-link/index.js
@@ -3,11 +3,11 @@ import Readme from '../readme'
 export default function ProjectLink({project, picture, handleClick}) {
   
   return (
-    <div className="portfolio__item-container">
-      <a href={project.link} className="portfolio__item">
+    <div className="portfolio__item-container" onClick={handleClick(project)}>
+      <div className="portfolio__item">
         <img src={picture} alt={project.alt} className="portfolio__img"/>
         <Readme readme={project.readme} />
-      </a>
+      </div>
       <a href={project.link} className="goto" target="_blank" rel="noreferrer">Go to page</a>
       <a href={project.github} className="goto" target="_blank" rel="noreferrer" >View code</a>
       <input type="button" className="goto" onClick={handleClick(project)} value="View here"/>
