7b1dd0e2541e7279366789d3a05b625809ba2a6b
Musa Bas <basmusa@icloud.com>
2021-09-06 09:59:38 +0100

    Linkedin link edited

--- src/components/links.js
+++ src/components/links.js
@@ -4,7 +4,7 @@ export default function Links() {
   return (
     <div className="icons">
       <a href="https://github.com/musabash"><FaGithub /></a>
-      <a href="https://www.linkedin.com/in/musa-bas-4984221bb/"><FaLinkedin /></a>
+      <a href="https://www.linkedin.com/in/musa-bas-79605621a/"><FaLinkedin /></a>
       <a href="https://twitter.com/musabas_"><FaTwitter /></a>
       <a href="mailto:musa.bas@musabas.com"><FaEnvelope /></a>
     </div>
